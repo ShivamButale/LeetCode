1193090346fcbe460d849337551f475fffc23c4a
Shivam Butale <shivbutale22@gmail.com>
2020-08-25 14:16:09 +0530

    Create Sum_of_Left_Leaves_1.cpp

---
+++ August-2020-LeetCoding-Challenge/24-August Sum of Left Leaves/Sum_of_Left_Leaves_1.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root, bool leftSide = false) {
+        if(root == NULL) return 0;
+        if(root->left == NULL && root->right == NULL && leftSide) return root->val;
+        return sumOfLeftLeaves(root->left, true) + sumOfLeftLeaves(root->right, false);
+    }
+};
