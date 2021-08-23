06f00776c5aff1039e4312e23508379f8d8a00e8
Shivam Butale <shivbutale22@gmail.com>
2021-08-23 11:24:48 +0530

    Create 168. Excel Sheet Column Title.py

---
+++ SolvedProblems/168. Excel Sheet Column Title.py
@@ -0,0 +1,22 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        dicti = {
+            0:'A', 1:'B', 2:'C',3:'D',
+                        4:'E', 5:'F', 6:'G',7:'H',
+                        8:'I', 9:'J', 10:'K', 11:'L',
+                        12:'M', 13:'N', 14:'O',15:'P',
+                        16:'Q', 17:'R', 18:'S',19:'T',
+                        20:'U', 21:'V', 22:'W',23:'X',
+                        24:'Y', 25:'Z'
+        }
+        if columnNumber <= 26:
+            return dicti[columnNumber-1]
+        string = ""
+        
+        while(columnNumber):
+            columnNumber -= 1
+            r = int(columnNumber % 26) 
+            string += dicti[(r)%26]
+            columnNumber = columnNumber // 26
+         
+        return string[::-1]
