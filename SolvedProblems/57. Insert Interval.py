9a231ab1f83c9487a2c906a5a6d39b1c0eb2b713
Shivam Butale <shivbutale22@gmail.com>
2025-03-29 23:32:03 -0400

    Create 57. Insert Interval.py

---
+++ SolvedProblems/57. Insert Interval.py
@@ -0,0 +1,18 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(intervals)):
+            # If new interval ends before current interval
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                # If new interval starts after current interval 
+                res.append(intervals[i])
+            else:
+                # There is an overlap 
+                newInterval[0] = min(newInterval[0], intervals[i][0])
+                newInterval[1] = max(newInterval[1], intervals[i][1])
+        res.append(newInterval)
+
+        return res 
